d749e7d0b023cf16dc803a8b92d47a57d8df8344
atcvetkov <63304837+atcvetkov@users.noreply.github.com>
2024-12-26 20:49:30 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,8 +10,18 @@
  * div   -> /
  */
 function calc (math_operation: string, var_1: number, var_2: number) {
-	
+    if (math_operation == "plus") {
+        basic.showNumber(var_1 + var_2)
+    } else if (math_operation == "minus") {
+        basic.showNumber(var_1 - var_2)
+    } else if (math_operation == "mult") {
+        basic.showNumber(var_1 * var_2)
+    } else if (math_operation == "div") {
+        basic.showNumber(var_1 / var_2)
+    } else {
+        basic.showString("Error!")
+    }
 }
 basic.forever(function () {
-	
+    calc("plus", 1, 1)
 })
