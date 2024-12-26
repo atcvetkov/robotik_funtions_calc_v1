588d7d54125cd47ea6ef05c974ee25859281eced
atcvetkov <63304837+atcvetkov@users.noreply.github.com>
2024-12-26 20:03:09 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+/**
+ * math_operation
+ * 
+ * plus  -> +
+ * 
+ * minus -> -
+ * 
+ * mult  -> *
+ * 
+ * div   -> /
+ */
+function calc (math_operation: string, var_1: number, var_2: number) {
+	
+}
+basic.forever(function () {
+	
+})
